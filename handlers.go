68c7e8cf68ffeddbd18e4c1c93241a8e9c9678bd
Karl Hepler <karl.hepler@bronto.com>
2019-08-12 08:05:57 -0600

    remove custom json marshal

--- handlers.go
+++ handlers.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
 )
 
 func createTodoHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	tw := todoWriter{Res: w, MarshalJSON: json.Marshal}
-	todosController.CreateTodo(tw, r.FormValue("label"))
+	todosController.CreateTodo(todoWriter{w}, r.FormValue("label"))
 }
--- writer.go
+++ writer.go
@@ -1,23 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 
 	"github.com/oldtimeguitarguy/todo/todo"
 )
 
-type MarshalJSON func(v interface{}) ([]byte, error)
-
 type todoWriter struct {
-	MarshalJSON MarshalJSON
-	Res         http.ResponseWriter
+	Res http.ResponseWriter
 }
 
 func (w todoWriter) Write(todo todo.Todo, status todo.Status) {
 	w.Res.Header().Set("Content-Type", "application/json")
 
-	jsonData, err := w.MarshalJSON(todo)
+	jsonData, err := json.Marshal(todo)
 	if err != nil {
 		log.Print("ERROR: " + err.Error())
 		w.Res.WriteHeader(500)
