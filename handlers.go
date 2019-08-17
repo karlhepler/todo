ab70462ea851e6536c5c849eb3ff00ed3fc3392b
Karl Hepler <karl.hepler@bronto.com>
2019-08-16 22:06:04 -0600

    removed needless todo struct

--- handlers.go
+++ handlers.go
@@ -10,12 +10,6 @@ func createTodoHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Para
 	todosController.CreateTodo(todoWriter{w}, r.FormValue("label"))
 }
 
-type Todo struct {
-	ID         interface{}
-	Label      string
-	IsComplete bool
-}
-
 func showTodoHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	todosController.GetTodoByID(todoWriter{w}, ps.ByName("id"))
 }
